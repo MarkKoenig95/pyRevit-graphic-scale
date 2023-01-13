c12dc768a62ce45e6c4b744a51a599affb901068
Mark Koenig <Markrkoenig95@gmail.com>
2023-01-13 12:44:26 -0600

    Add: Metric Scale View Title Family Import Ability Polish to UX

--- pyRevit.tab/OTIE.panel/Graphic Scale.pushbutton/graphic_scale_utilities.py
+++ pyRevit.tab/OTIE.panel/Graphic Scale.pushbutton/graphic_scale_utilities.py
@@ -1,29 +1,45 @@
-from pyrevit import HOST_APP, DB
+from pyrevit import HOST_APP, DB, forms
 from System import Guid
 import os
 
 one_inch = 0.083333333334
 
-imperial_sizes = [(one_inch * 0.25, "o_0.25in Scale Text"),
-				 (one_inch * 0.375, "o_0.375in Scale Text"),
-				 (one_inch * 0.5, "o_0.5in Scale Text"),
-				 (one_inch * 0.75, "o_0.75in Scale Text"),
-				 (one_inch * 1, "o_1.0in Scale Text"),
-				 (one_inch * 1.5, "o_1.5in Scale Text"),
-				 (one_inch * 2, "o_2.0in Scale Text"),
-				 (one_inch * 2.25, "o_2.25in Scale Text")]
+imperial_sizes = [(one_inch * 0.25  ,  "o_0.25in Scale Text"),
+				  (one_inch * 0.375 , "o_0.375in Scale Text"),
+				  (one_inch * 0.5   ,   "o_0.5in Scale Text"),
+				  (one_inch * 0.75  ,  "o_0.75in Scale Text"),
+				  (one_inch * 1     ,   "o_1.0in Scale Text"),
+				  (one_inch * 1.5   ,   "o_1.5in Scale Text"),
+				  (one_inch * 2     ,   "o_2.0in Scale Text"),
+				  (one_inch * 2.25  ,  "o_2.25in Scale Text")]
+
+metric_sizes = [(10 , "o_1 cm Scale Text"),
+				(20 , "o_2 cm Scale Text"),
+				(30 , "o_3 cm Scale Text"),
+				(40 , "o_4 cm Scale Text"),
+				(50 , "o_5 cm Scale Text")]
 
 def get_parameter_names():
 	parameter_names = []
+
 	for size in imperial_sizes:
 		parameter_name = size[1]
 		parameter_names.append(parameter_name)
+		
+	for size in metric_sizes:
+		parameter_name = size[1]
+		parameter_names.append(parameter_name)
+
 	return parameter_names
 
-def add_shared_parameters(app, doc):
+def get_support_file_path(file_name):
 	absolute_path = os.path.dirname(os.path.abspath(__file__))
-	relative_path = "supporting_files\graphic_scale_shared_parameters.txt"
-	graphic_scale_SPFN = os.path.join(absolute_path, relative_path)
+	relative_path = "supporting_files"
+	file_path = os.path.join(absolute_path, relative_path, file_name)
+	return file_path
+
+def add_shared_parameters(app, doc):
+	graphic_scale_SPFN = get_support_file_path("graphic_scale_shared_parameters.txt")
 
 	shared_parameters_group_name = "Graphic Scale"
 
@@ -59,8 +75,25 @@ def add_shared_parameters(app, doc):
 	t.Commit()
 	t.Dispose()
 
+def add_view_title_family(doc):
+	view_title_family_name = "oc_View Title"
+	t = DB.Transaction(doc, 'Add view title family')
+	t.Start()
+	
+	view_title_family_path = get_support_file_path(view_title_family_name + ".rfa")
+	was_successful = doc.LoadFamily(view_title_family_path)
 
-def get_segment_length_text_from_scale_value(scale_value, sheet_segment_length):
+	t.Commit()
+	t.Dispose()
+
+	if(not was_successful):
+		
+		forms.alert('There was an error loading the view title family. '
+					'Maybe there is an Annotation Symbol with the same name in your project already.\n\n'
+					'If you would like to replace this family please rename the old "' + view_title_family_name + '" family and retry.'
+					, ok=True, yes=False, no=False)
+
+def get_imperial_text_from_scale_value(scale_value, sheet_segment_length):
 	segment_length_text = ''
 	
 	drawing_segment_length = (scale_value * sheet_segment_length)
@@ -80,20 +113,27 @@ def get_segment_length_text_from_scale_value(scale_value, sheet_segment_length):
 	return segment_length_text
 
 def update_one_scale_value(view):
-	scale_value = view.LookupParameter("View Scale").AsInteger()
+	scale_value = view.LookupParameter("Scale Value    1:").AsInteger()\
+	
+	for size in imperial_sizes:
+		segment_length = size[0]
+		parameter_name = size[1]
 
-	for size_index in range(0,len(imperial_sizes)):
-		segment_length = imperial_sizes[size_index][0]
-		parameter_name = imperial_sizes[size_index][1]
-
-		segment_length_text = get_segment_length_text_from_scale_value(scale_value, segment_length)
+		segment_length_text = get_imperial_text_from_scale_value(scale_value, segment_length)
 			
 		o_scale_value_param = view.LookupParameter(parameter_name)
 			
 		o_scale_value_param.Set(segment_length_text)
-		
-	# Add another for loop for metric values here
 
+	for size in metric_sizes:
+		segment_length = size[0]
+		parameter_name = size[1]
+
+		segment_length_text = str(int(scale_value * segment_length))
+			
+		o_scale_value_param = view.LookupParameter(parameter_name)
+		o_scale_value_param.Set(segment_length_text)
+		
 def check_if_has_shared_parameters(view_collector):
 	has_shared_parameters = True
 	parameter_names = get_parameter_names()
@@ -122,7 +162,6 @@ def update_all_scale_values(app, doc, view_collector_param):
 		update_one_scale_value(view)
 	t.Commit()
 	t.Dispose()
-
 
 class GraphicScaleUpdater(DB.IUpdater):
 	def __init__(self, application_id):
--- pyRevit.tab/OTIE.panel/Graphic Scale.pushbutton/script.py
+++ pyRevit.tab/OTIE.panel/Graphic Scale.pushbutton/script.py
@@ -1,12 +1,27 @@
-from pyrevit import DB
-import os
-from graphic_scale_utilities import update_all_scale_values, register_graphic_scale_updater
+from pyrevit import DB, forms
+from graphic_scale_utilities import add_view_title_family, update_all_scale_values, register_graphic_scale_updater, check_if_has_shared_parameters
 
 __title__ = "Graphic\nScale"
 
 app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
-sel = __revit__.ActiveUIDocument.Selection
 
-update_all_scale_values(app, doc, None)
-register_graphic_scale_updater()
+should_set_up = True
+
+view_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Views).WhereElementIsNotElementType()
+has_shared_parameters = check_if_has_shared_parameters(view_collector)
+if not has_shared_parameters:
+    should_set_up = forms.alert("Would you like to set up this project to use an automatic view title graphic scale updater? "
+                    "This add-in only works if users have it installed, otherwise graphic scales may not be updated "
+                    "when a user changes a view's scale.\n\n"
+
+                    "Do you still want to set up this extension?",
+                    ok=False, yes=True, no=True)
+
+if should_set_up:
+    add_view_title_family(doc)
+    update_all_scale_values(app, doc, view_collector)
+    register_graphic_scale_updater()
+    forms.alert("This project is now set up to use the automatic view title graphic scale updater. "
+                "Please make sure to use the correct family for your view titles and feel free to "
+                "edit the view title family to your project specific needs.")
